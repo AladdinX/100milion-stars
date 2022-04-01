ffa1d7dfeb1777e2de94c8c24d34db629932b355
AladdinX <Alaa.aldroubi@gmail.com>
2022-04-01 23:14:41 +0200

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,10 @@
+
+function setup() {
+  createCanvas(windowWidth, windowHeight);
+  background(1);
+  strokeWeight(random());
+  for(let i=0;i<random(10000000);i++){
+    stroke(random(255),random(255),random(255),random(100));
+    ellipse(random(width),random(height),random());
+  }
+}
