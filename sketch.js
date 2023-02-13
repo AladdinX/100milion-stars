8cb64423d40af14d7e5b1f1e6e378f35cea3b6ab
Alaa alden Aldroubi <Alaa.aldroubi@gmail.com>
2023-02-13 19:16:50 +0100

    Stop Refreshing the page and only reRender
    with some edits for more readable code !

--- sketch.js
+++ sketch.js
@@ -1,16 +1,17 @@
 
 function setup() {
-  //
   createCanvas(windowWidth, windowHeight);
-  background(1);
+  background('black');
   strokeWeight(random());
-  
+  createStars();
+}
+function mousePressed() {
+ createStars();
+}
+
+const createStars = () => {
   for(let i=0;i<random(10000000);i++){
     stroke(random(255),random(255),random(255),random(100));
     ellipse(random(width),random(height),random());
   }
-}
-
-  function mousePressed() {
- window.location.reload()
 }
